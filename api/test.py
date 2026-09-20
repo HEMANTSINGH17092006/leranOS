12bc2c7571dc793f4321e2ea0af36e1a87319bb1
Hemant <hemant11402chs@gmail.com>
2026-09-20 22:34:16 +0530

    Export pure Flask WSGI app in api/index.py for standard Vercel Python runtime

--- api/test.py
+++
@@ -1,9 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-import sys
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(f'Vercel Python OK. Python: {sys.version}'.encode('utf-8'))
